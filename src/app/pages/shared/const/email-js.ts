8e36540b2e34e3281161fde27baa3b8131799349
ionitaandrei66 <ionita_andrei_2002@yahoo.com>
2025-12-29 12:54:42 +0200

    ,.

--- src/app/pages/shared/const/email-js.ts
+++ src/app/pages/shared/const/email-js.ts
@@ -4,8 +4,4 @@ export const EMAILJS_CONFIG = Object.freeze({
   templateId: 'template_kz2hfqw',
   publicKey: 'fWCwNDXWBZCf4yauk',
 } as const);
-export const EMAILJS_SERVICE_ID = EMAILJS_CONFIG.serviceId;
-export const EMAILJS_TEMPLATE_ID = EMAILJS_CONFIG.templateId;
-export const EMAILJS_PUBLIC_KEY = EMAILJS_CONFIG.publicKey;
 
-//todo make it more secure && rename
